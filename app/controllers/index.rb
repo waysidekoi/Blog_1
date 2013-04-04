1ee95632dcc237a0d9ff0b8466214cfce2769a75
Apprentice <apprentice@devbootcamp.com>
2013-04-04 14:23:09 -0700

    index page css&html complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,6 @@
 # just have the index route, which will display homepage
 
-# get '/' do
-#   # Look in app/views/index.erb
-#   erb :index
-# end
+get '/' do
+  # Look in app/views/index.erb
+  erb :index
+end
